8fe1c76f0208ee7eb91b6de95f745d077b491f37
Mc Londono <mc.londono@uniandes.edu.co>
2019-11-15 09:15:46 -0500

    module info

---
+++ src/module-info.java
@@ -0,0 +1,1 @@
+module caso3_mc.londono_cf.infante {exports clienteProtocolos;exports servidorConSeguridad;exports servidorSinSeguridad;requires GLoad13;requires java.management;requires javax.xml.bind;requires org.bouncycastle.pkix;requires org.bouncycastle.provider;}
